f580d954da3ef393915e029256c168ab402f19df
allenspark10 <eaayline5@gmail.com>
2026-02-01 14:59:15 +0530

    Upload addon.js

---
+++ backend/src/config/addon.js
@@ -0,0 +1,6 @@
+javascript
+module.exports = {
+  chunkSize: 5 * 1024 * 1024, // 5MB chunks
+  maxAge: 30 * 60 * 1000,      // 30 minutes
+  cleanupInterval: 5 * 60 * 1000, // Check every 5 minutes
+};
